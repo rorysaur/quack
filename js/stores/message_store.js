54cb08c36680ab50d030ddcaae8b5e67128ea922
Jack Noble <jack.noble@gmail.com>
2015-03-11 22:52:49 -0700

    create message store

--- js/stores/message_store.js
+++ js/stores/message_store.js
@@ -1,7 +1,28 @@
 var EventEmitter = require('events').EventEmitter;
 var assign = require('object-assign');
-
+var AppDispatcher = require('../dispatcher/app_dispatcher')
+var _messages = []
 var MessageStore = assign({}, EventEmitter.prototype, {
+
+  all: function() {
+    return _messages
+  },
+
+  setInitialMessages: function(messages) {
+    _messages = messages
+  }
 });
 
+MessageStore.dispatchToken = AppDispatcher.register(function(action) {
+  if (action.type === "NewMessage") {
+    message = {
+      id: MessageStore.all().length + 1,
+      timestamp: new Date().getTime(),
+      user: 'jack',
+      text: action.text
+    }
+    _messages.push(message)
+    MessageStore.emit("change")
+  }
+})
 module.exports = MessageStore;
