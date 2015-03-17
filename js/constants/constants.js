f3229b9c5f611ee3e94228501891448130a4ddd7
Jack Noble <jack.noble@gmail.com>
2015-03-16 18:26:59 -0700

    add action type

--- js/constants/constants.js
+++ js/constants/constants.js
@@ -3,6 +3,7 @@ var keyMirror = require('react/lib/keyMirror');
 module.exports = {
   ActionTypes: keyMirror({
     NEW_MESSAGE: null,
-    RENAME_LOCAL_USER: null
+    RENAME_LOCAL_USER: null,
+    CHANGE_SETTING: null,
   })
 };
