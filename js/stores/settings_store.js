40bcf257e0edc68337ebffba0460b7df4705f495
Jack Noble <jack.noble@gmail.com>
2015-03-16 18:27:09 -0700

    add settings store

---
+++ js/stores/settings_store.js
@@ -0,0 +1,24 @@
+var EventEmitter = require('events').EventEmitter;
+var assign = require('object-assign');
+var AppDispatcher = require('../dispatcher/app_dispatcher');
+var ActionTypes = require('../constants/constants').ActionTypes;
+
+var _settings = {
+  Command: ":"
+};
+
+var SettingsStore = assign({}, EventEmitter.prototype, {
+  commandCharacter: function() {
+    return _settings.Command;
+  }
+});
+
+SettingsStore.dispatchToken = AppDispatcher.register(function(action) {
+  if (action.type === ActionTypes.CHANGE_SETTING) {
+    if (_settings.hasOwnProperty(action.data.variable)) {
+      _settings[action.data.variable] = action.data.value;
+    }
+  }
+});
+
+module.exports = SettingsStore;
