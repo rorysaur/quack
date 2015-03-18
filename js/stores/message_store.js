53841ec093aecd29fd1af56371a92d820ee0bb37
Jack Noble <jack.noble@gmail.com>
2015-03-17 22:37:12 -0700

    add action handler in store

--- js/stores/message_store.js
+++ js/stores/message_store.js
@@ -8,13 +8,15 @@ var _messages = [
     id: 1,
     timestamp: new Date().getTime(),
     user: 'rory',
-    text: 'hihi'
+    text: 'hihi',
+    local: false
   },
   {
     id: 2,
     timestamp: new Date().getTime(),
     user: 'iz',
-    text: 'omg hi'
+    text: 'omg hi',
+    local: false
   }
 ];
 var MessageStore = assign({}, EventEmitter.prototype, {
@@ -22,18 +24,39 @@ var MessageStore = assign({}, EventEmitter.prototype, {
   all: function() {
     return _messages;
   },
+
+  local: function() {
+    var localMessages = [];
+    _messages.forEach(function(message) {
+      if (message.local === true) {
+        localMessages.push(message);
+      }
+    });
+
+    return localMessages;
+  }
 });
 
 MessageStore.dispatchToken = AppDispatcher.register(function(action) {
-  if (action.type === ActionTypes.NEW_MESSAGE) {
-    var message = {
-      id:  MessageStore.all().length,
-      timestamp: action.data.timestamp,
-      user: action.data.user,
-      text: action.data.text
-    };
-    _messages.push(message);
-    MessageStore.emit('change');
+  switch(action.type) {
+    case ActionTypes.NEW_MESSAGE:
+      var message = {
+        id:  MessageStore.all().length,
+        timestamp: action.data.timestamp,
+        user: action.data.user,
+        text: action.data.text,
+        local: action.data.local
+      };
+      _messages.push(message);
+      MessageStore.emit('change');
+      break;
+
+    case ActionTypes.EDIT_LAST_MESSAGE:
+      var localMessages = MessageStore.local();
+      var lastMessage = localMessages[localMessages.length - 1];
+      lastMessage.text = lastMessage.text.replace(action.data.find, action.data.replaceWith);
+      MessageStore.emit('change');
+      break;
   }
 });
 module.exports = MessageStore;
