7cedc2f49739848053ca54ee715d1ee589fd2f40
Jack Noble <jack.noble@gmail.com>
2015-03-29 12:33:31 -0700

    make sure tests are correct in any timezone

--- js/utils/__tests__/time_presenter_test.js
+++ js/utils/__tests__/time_presenter_test.js
@@ -9,37 +9,39 @@ describe('TimePresenter', function() {
   var oneDay = 86400000;
   var oneWeek = 604800000;
 
+  var offset = new Date().getTimezoneOffset() - 420;
+
   beforeEach(function() {
     TimePresenter = require('../time_presenter');
   });
 
   it("displays times within the last minute as 'just now'", function() {
-    var displayTime = TimePresenter.presentMessageTime(messageTime, new Date(messageTime + thirtySeconds));
+    var displayTime = TimePresenter.presentMessageTime(messageTime, new Date(messageTime + thirtySeconds - offset));
     expect(displayTime).toBe('just now');
   });
 
   it("displays times from within the hour as 'x minutes ago'", function() {
-    var displayTime = TimePresenter.presentMessageTime(messageTime, new Date(messageTime + fortyFiveMinutes));
+    var displayTime = TimePresenter.presentMessageTime(messageTime, new Date(messageTime + fortyFiveMinutes - offset));
     expect(displayTime).toBe('45 minutes ago');
   });
 
   it('displays times from earlier in the day with the time', function() {
-    var displayTime = TimePresenter.presentMessageTime(messageTime, new Date(messageTime + (oneHour * 2)));
+    var displayTime = TimePresenter.presentMessageTime(messageTime, new Date(messageTime + (oneHour * 2) - offset));
     expect(displayTime).toBe('9:47 PM');
   });
 
   it("displays times from yesterday with 'Yesterday at X:XX'", function() {
-    var displayTime = TimePresenter.presentMessageTime(messageTime, new Date(messageTime + (oneHour * 5)));
+    var displayTime = TimePresenter.presentMessageTime(messageTime, new Date(messageTime + (oneHour * 5) - offset));
     expect(displayTime).toBe('Yesterday 9:47 PM');
   });
 
   it('displays the day and time after more than two days have passed', function() {
-    var displayTime = TimePresenter.presentMessageTime(messageTime, new Date(messageTime + (oneDay * 3)));
+    var displayTime = TimePresenter.presentMessageTime(messageTime, new Date(messageTime + (oneDay * 3) - offset));
     expect(displayTime).toBe('Saturday 9:47 PM');
   });
 
   it('displays the date and month after more than a week has passed', function() {
-    var displayTime = TimePresenter.presentMessageTime(messageTime, new Date(messageTime + (oneWeek * 2)));
+    var displayTime = TimePresenter.presentMessageTime(messageTime, new Date(messageTime + (oneWeek * 2) - offset));
     expect(displayTime).toBe('March 28');
   });
 });
