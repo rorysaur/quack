2c15821cdb4f9c5f3ff77004c71d3380bb2760f1
Jack Noble <jack.noble@gmail.com>
2015-03-29 13:18:05 -0700

    move clock the channel_messages layer

--- js/components/channel_messages.jsx
+++ js/components/channel_messages.jsx
@@ -1,15 +1,17 @@
 var React = require('react');
 var Message = require('./message.jsx');
 var MessageStore = require('../stores/message_store');
+var TimePresenter = require('../utils/time_presenter');
 
 var ChannelMessages = React.createClass({
   render: function() {
     var messageNodes = this.state.messages.map(function(message) {
+      var displayTime = TimePresenter.presentMessageTime(message.timestamp, this.state.currentTime);
       return (
-        <Message message={message}>
+        <Message message={message} displayTime={displayTime}>
         </Message>
       );
-    });
+    }.bind(this));
     return (
       <div className='channel-messages'>
         <h1>You are in a Quack channel.</h1>
@@ -19,15 +21,29 @@ var ChannelMessages = React.createClass({
   },
 
   getInitialState: function() {
-    return {messages: MessageStore.all()};
+    return {
+      messages: MessageStore.all(),
+      currentTime: new Date()
+    };
   },
 
   componentDidMount: function() {
     MessageStore.on('change', this.messageStoreChange);
+    this.startClock();
+  },
+
+  componentWillUnmount: function() {
+    clearInterval(this.clockId);
   },
 
   messageStoreChange: function() {
     this.setState({messages: MessageStore.all()});
+  },
+
+  startClock: function() {
+    this.clockId = setInterval(function() {
+      this.setState({currentTime: new Date()});
+    }.bind(this), 60000);
   }
 
 });
--- js/components/message.jsx
+++ js/components/message.jsx
@@ -4,27 +4,16 @@ var TimePresenter = require('../utils/time_presenter');
 var Message = React.createClass({
   render: function() {
     var message = this.props.message;
-    var displayTime = TimePresenter.presentMessageTime(message.timestamp, this.state.currentTime);
     return (
       <div className="message">
         <div className="sender-stamp">
           <div className="message-sender">{message.user}</div>
-          <div className="message-time">{displayTime}</div>
+          <div className="message-time">{this.props.displayTime}</div>
         </div>
         <div className="message-text">{message.text}</div>
       </div>
     );
   },
-
-  getInitialState: function() {
-    return { currentTime: new Date() };
-  },
-
-  componentDidMount: function() {
-    setInterval(function() {
-      this.setState({currentTime: new Date()});
-    }.bind(this), 60000);
-  }
 });
 
 module.exports = Message;
