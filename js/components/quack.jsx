a94a3c53d6d0a8a0cd2209a7c8b8d7f3b2a33be1
Satish Kunisi <kunisi@gmail.com>
2015-04-02 20:05:49 -0700

    Add link to about component

--- js/components/quack.jsx
+++ js/components/quack.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var About = require('./about.jsx');
 var Chat = require('./chat.jsx');
 var Router = require('react-router');
 var RouteHandler = Router.RouteHandler;
@@ -10,7 +11,7 @@ var Quack = React.createClass({
       <div className='quack'>
         <nav>
           <ul>
-            <li><Link to="login">Login</Link></li>
+            <li><Link to="about">About</Link></li>
             <li><Link to="chat">Chat</Link></li>
           </ul>
         </nav>
