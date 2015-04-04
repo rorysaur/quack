467433365322c377fb31b4a752bb6ffacd3e26a8
Satish Kunisi <kunisi@gmail.com>
2015-04-03 17:21:11 -0700

    Use semantic tags in about component

--- js/components/about.jsx
+++ js/components/about.jsx
@@ -3,9 +3,11 @@ var React = require('react');
 var About = React.createClass({
   render: function () {
     return (
-      <div className="about">
-        A humble but ambitious clone of Slack, the chat app, using React, Flux, and Firebase.
-      </div>
+      <article className="about">
+        <p>
+          A humble but ambitious clone of Slack, the chat app, using React, Flux, and Firebase.
+        </p>
+      </article>
     );
   }
 });
