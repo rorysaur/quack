4dd4dd81719ae83cdfdaafbc4b9d89887792715f
rorysaur <ou.rory@gmail.com>
2015-07-20 22:15:34 -0700

    add user fields and validations

--- web/models/user.ex
+++ web/models/user.ex
@@ -2,13 +2,17 @@ defmodule Quack.User do
   use Quack.Web, :model
 
   schema "users" do
+    field :email, :string
     field :name, :string
+    field :password_encrypted, :string
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
     has_many :messages, Message
 
     timestamps
   end
 
-  @required_fields ~w(name)
+  @required_fields ~w(email name password password_confirmation)
   @optional_fields ~w()
 
   @doc """
@@ -20,5 +24,8 @@ defmodule Quack.User do
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
+    |> validate_unique(:email, on: Quack.Repo, downcase: true)
+    |> validate_length(:password, min: 6)
+    |> validate_confirmation(:password, message: "passwords do not match")
   end
 end
