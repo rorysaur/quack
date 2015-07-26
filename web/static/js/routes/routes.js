616ce213b49ae2e58f4b3c8e09de52f4399d7d61
rorysaur <ou.rory@gmail.com>
2015-07-25 18:24:40 -0700

    add Register route

--- web/static/js/routes/routes.js
+++ web/static/js/routes/routes.js
@@ -6,10 +6,12 @@ var About = require('../components/about.jsx');
 var Quack = require('../components/quack.jsx');
 var Chat = require('../components/chat.jsx');
 var Login = require('../components/login.jsx');
+var Register = require('../components/register.jsx');
 
 var routes = (
   <Route path='/' handler={Quack}>
     <DefaultRoute name="login" handler={Login} />
+    <Route name="register" handler={Register} />
     <Route name="chat" handler={Chat} />
     <Route name="about" handler={About}/>
   </Route>
