0aa9291b5bcd4d53d151012fb85123b6db3839cd
rorysaur <ou.rory@gmail.com>
2015-07-25 18:21:27 -0700

    add comeonin for bcrypt

--- mix.exs
+++ mix.exs
@@ -34,6 +34,7 @@ defmodule Quack.Mixfile do
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 1.1"},
      {:phoenix_live_reload, "~> 0.4.3", only: :dev},
+     {:comeonin, "~> 1.0"},
      {:cowboy, "~> 1.0"}]
   end
 end
